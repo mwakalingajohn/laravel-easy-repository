62eb9776f1a75030816522cd88a0b14c0647fce7
Yaz3 <59905886+yaza-putu@users.noreply.github.com>
2021-10-28 00:33:16 +0800

    Fixing bug The "policy" option does not exist
    Fixing bug  The "policy" option does not exist when call php artisan make

--- src/Commands/ModelMakeCommand.php
+++ src/Commands/ModelMakeCommand.php
@@ -89,6 +89,7 @@ class ModelMakeCommand extends ConsoleModelMakeCommand
             ['pivot', 'p', InputOption::VALUE_NONE, 'Indicates if the generated model should be a custom intermediate table model'],
             ['resource', 'r', InputOption::VALUE_NONE, 'Indicates if the generated controller should be a resource controller'],
             ['api', null, InputOption::VALUE_NONE, 'Indicates if the generated controller should be an API controller'],
+            ['policy', null, InputOption::VALUE_NONE, 'Create a new policy for the model'],
         ];
     }
 }
